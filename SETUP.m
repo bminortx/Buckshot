a9f618011ba6dd5dde0bbedbe0f6941c814e8286
bminortx <gallimatrix@gmail.com>
2015-12-06 15:31:53 -0700

    cleanup

--- SETUP.m
+++ SETUP.m
@@ -3,7 +3,6 @@ function [ new_class_ptr ] = SETUP(boolCreatePtr)
 %function in MATLAB. The boolCreatePtr that SETUP takes as an argument lets the
 %user choose whether or not they want to create a bullet class object (true), or
 %merely link the libraries to MATLAB for ease of coding (false).
-%
 
 %The Bullet-MATLAB interface
 addpath(genpath('bulletComponents'));
