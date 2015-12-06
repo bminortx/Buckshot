63e208373608100bc79bfe75abaa06b6d1f04299
bminortx <gallimatrix@gmail.com>
2015-12-06 10:30:23 -0700

    Got rid of ninja for script

--- createMexFiles.sh
+++ createMexFiles.sh
@@ -4,17 +4,11 @@ set -u
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-INTERFACE_DIR=$SCRIPT_DIR/bulletInterface
+INTERFACE_DIR=$SCRIPT_DIR/bulletComponents
 
 mkdir -p $INTERFACE_DIR/build
 cd $INTERFACE_DIR/build
 
-# I use ninja, but just get rid of -GNinja for make instead
-if [[ $1 ]]; then
-		cmake -GNinja $INTERFACE_DIR -DSCENEGRAPH_INTERFACE=ON
-else
-		cmake -GNinja $INTERFACE_DIR -DSCENEGRAPH_INTERFACE=OFF
-fi
-
-ninja -j4
+cmake $INTERFACE_DIR
+make -j4
 
