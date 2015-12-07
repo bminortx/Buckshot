04f41592a57681aee941579571334d40b1d72f18
bminortx <gallimatrix@gmail.com>
2015-12-06 21:42:45 -0700

    Changed the program name

--- bulletComponents/bullet_interface.m
+++ bulletComponents/bullet_interface.m
@@ -1,7 +1,7 @@
 classdef bullet_interface < handle
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %%%% MATLAB class wrapper to the Bullet C++ architecture.
-  %%%% Any method with 'bulletInterface(...)'
+  %%%% Any method with 'buckshot(...)'
   %%%% represents a call to the Bullet wrapper for MATLAB.
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   
@@ -26,7 +26,7 @@ classdef bullet_interface < handle
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     function this = bullet_interface(varargin)
-      this.bulletHandle = bulletInterface('new', varargin{:});
+      this.bulletHandle = buckshot('new', varargin{:});
       this.Terrain = [];
       this.gui.draw = false;           % bool to draw Sim
       this.gui.quit = false;           % bool to quit Sim
@@ -44,7 +44,7 @@ classdef bullet_interface < handle
     end
     
     function delete(this)
-      bulletInterface('delete', this.bulletHandle);
+      buckshot('delete', this.bulletHandle);
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -70,7 +70,7 @@ classdef bullet_interface < handle
           restitution = Shape.GetRestitution();
           position = Shape.GetPosition();
           rotation = Shape.GetRotation();
-          id = bulletInterface('AddShape', ...
+          id = buckshot('AddShape', ...
             this.bulletHandle, type, CubeDim(1), CubeDim(2), CubeDim(3), ...
             mass, restitution, position, rotation);
           Shape.SetID(id);
@@ -82,7 +82,7 @@ classdef bullet_interface < handle
           restitution = Shape.GetRestitution();
           position = Shape.GetPosition();
           rotation = Shape.GetRotation();
-          id = bulletInterface('AddShape', ...
+          id = buckshot('AddShape', ...
             this.bulletHandle, type, radius, mass, restitution, ...
             position, rotation);
           Shape.SetID(id);
@@ -95,7 +95,7 @@ classdef bullet_interface < handle
           restitution = Shape.GetRestitution();
           position = Shape.GetPosition();
           rotation = Shape.GetRotation();
-          id = bulletInterface('AddShape', ...
+          id = buckshot('AddShape', ...
             this.bulletHandle, type, radius, height, mass,...
             restitution, position, rotation);
           Shape.SetID(id);
@@ -120,7 +120,7 @@ classdef bullet_interface < handle
       max_ht = extrema(3, 2);
       sze = size(heightmap{1});
       normal = Terrain.GetNormal();
-      id = bulletInterface('AddTerrain', ...
+      id = buckshot('AddTerrain', ...
         this.bulletHandle, sze(1), sze(2), grad, min_ht, max_ht, heightmap{1}, ...
         heightmap{2}, heightmap{3}, normal);
       this.Terrain.SetID(id);
@@ -136,7 +136,7 @@ classdef bullet_interface < handle
         parameters = RayVehicles{i}.GetParameters();
         position = RayVehicles{i}.GetPosition();
         rotation = RayVehicles{i}.GetRotation();
-        id = bulletInterface('AddRaycastVehicle', this.bulletHandle, ...
+        id = buckshot('AddRaycastVehicle', this.bulletHandle, ...
           parameters, position, rotation);
         RayVehicles{i}.SetID(id);
       end
@@ -165,14 +165,14 @@ classdef bullet_interface < handle
         if strcmp(type, 'PointToPoint_one')==true,
           id_A = Constraint.Shape_A.GetID();
           pivot_in_A = Constraint.pivot_in_A;
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'PointToPoint_one', id_A, pivot_in_A);
         elseif strcmp(type, 'PointToPoint_two')==true,
           id_A = Constraint.Shape_A.GetID();
           pivot_in_A = Constraint.pivot_in_A;
           id_B = Constraint.Shape_B.GetID();
           pivot_in_B = Constraint.pivot_in_B;
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'PointToPoint_two', id_A, id_B, pivot_in_A, pivot_in_B);
           
           % Hinge
@@ -180,7 +180,7 @@ classdef bullet_interface < handle
           id_A = Constraint.Shape_A.GetID();
           transform_A = Constraint.transform_A;
           limits = Constraint.GetLimits();
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'Hinge_one_transform', id_A, transform_A, limits);
           
         elseif strcmp(type, 'Hinge_two_transform')==true,
@@ -189,7 +189,7 @@ classdef bullet_interface < handle
           id_B = Constraint.Shape_B.GetID();
           transform_B = Constraint.transform_B;
           limits = Constraint.GetLimits();
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'Hinge_two_transform', id_A, id_B, transform_A, transform_B, limits);
           
         elseif strcmp(type, 'Hinge_one_pivot')==true,
@@ -197,7 +197,7 @@ classdef bullet_interface < handle
           pivot_in_A = Constraint.pivot_in_A;
           axis_in_A = Constraint.axis_in_A;
           limits = Constraint.GetLimits();
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'Hinge_one_pivot', id_A, pivot_in_A, axis_in_A, limits);
           
         elseif strcmp(type, 'Hinge_two_pivot')==true,
@@ -208,7 +208,7 @@ classdef bullet_interface < handle
           pivot_in_B = Constraint.pivot_in_B;
           axis_in_B = Constraint.axis_in_B;
           limits = Constraint.GetLimits();
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'Hinge_two_pivot', id_A, id_B, pivot_in_A, pivot_in_B, ...
             axis_in_A, axis_in_B, limits);
           
@@ -222,7 +222,7 @@ classdef bullet_interface < handle
           damping = Constraint.GetDamping;
           stiffness = Constraint.GetStiffness;
           steering_angle = Constraint.GetSteeringAngle;
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'Hinge2', id_A, id_B, Anchor, Axis_1, ...
             Axis_2, damping, stiffness, steering_angle);
           
@@ -231,7 +231,7 @@ classdef bullet_interface < handle
           id_A = Constraint.Shape_A.GetID();
           transform_A = Constraint.GetTransform_A();
           limits = Constraint.GetLimits();
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'SixDOF_one', id_A, transform_A, limits);
         elseif strcmp(type, 'SixDOF_two')==true,
           id_A = Constraint.Shape_A.GetID();
@@ -239,7 +239,7 @@ classdef bullet_interface < handle
           id_B = Constraint.Shape_B.GetID();
           transform_B = Constraint.GetTransform_B();
           limits = Constraint.GetLimits();
-          id = bulletInterface('AddConstraint', this.bulletHandle, ...
+          id = buckshot('AddConstraint', this.bulletHandle, ...
             'SixDOF_two', id_A, id_B, transform_A, transform_B, limits);
         end
         Constraint.SetID(id);
@@ -259,7 +259,7 @@ classdef bullet_interface < handle
         steering = Compound.GetSteering();
         force = Compound.GetForce();
         id = Compound.GetID();
-        bulletInterface('CommandCompound', this.bulletHandle, 'Vehicle', ...
+        buckshot('CommandCompound', this.bulletHandle, 'Vehicle', ...
           id, steering, force);
       end
     end
@@ -274,7 +274,7 @@ classdef bullet_interface < handle
       if this.gui.draw,
         command = Vehicle.PushCommand();
         id = Vehicle.GetID();
-        bulletInterface('CommandRaycastVehicle', this.bulletHandle, ...
+        buckshot('CommandRaycastVehicle', this.bulletHandle, ...
           id, command.steering, command.force);
       end
     end
@@ -283,20 +283,20 @@ classdef bullet_interface < handle
     function [steering, force, lin_vel, ang_vel] = GetMotionState(this, Vehicle)
       id = Vehicle.GetID();
       [steering, force, lin_vel, ang_vel] = ...
-        bulletInterface('GetMotionState', this.bulletHandle, id);
+        buckshot('GetMotionState', this.bulletHandle, id);
     end
     
     function SetToGround(this, Vehicle, x_coord, y_coord)
       id = Vehicle.GetID();
       % Get our new position
-      [position] = bulletInterface('SetToGround', this.bulletHandle, id, ...
+      [position] = buckshot('SetToGround', this.bulletHandle, id, ...
         x_coord, y_coord);
       % Set our new position
       [body_position, body_rotation,...
         wheel_fl_pos, wheel_fl_rot,...
         wheel_fr_pos, wheel_fr_rot,...
         wheel_bl_pos, wheel_bl_rot,...
-        wheel_br_pos, wheel_br_rot] = bulletInterface('GetTransform', ...
+        wheel_br_pos, wheel_br_rot] = buckshot('GetTransform', ...
         this.bulletHandle, 'RaycastVehicle', Vehicle.GetID());
       Vehicle.SetTransform(body_position, body_rotation,...
         wheel_fl_pos, wheel_fl_rot,...
@@ -304,23 +304,10 @@ classdef bullet_interface < handle
         wheel_bl_pos, wheel_bl_rot,...
         wheel_br_pos, wheel_br_rot);
     end
-    
-%     function [states, end_position, end_rotation, end_lin_vel, ...
-%               end_ang_vel, grounded] =  
-%         SpeedSim(this, Vehicle, start_pose, start_rot, start_lin_vel, ...
-%                  start_ang_vel, engine_commands, steering_commands);
-%       id = Vehicle.GetID();
-%       [states, end_position, end_rotation, end_lin_vel, ...
-%        end_ang_vel, grounded] = 
-%       bulletInterface('SpeedSim', this.bulletHandle, id, start_pose, ...
-%                            start_rot, start_lin_vel,... 
-%                            start_ang_vel, engine_commands, steering_commands, ...
-%                            numel(steering_commands));
-%     end
 
     function ResetVehicle(this, Vehicle, start_pose, start_rot)
       id = Vehicle.GetID();
-      bulletInterface('ResetVehicle', this.bulletHandle, id, start_pose, start_rot);
+      buckshot('ResetVehicle', this.bulletHandle, id, start_pose, start_rot);
     end
     
     function RapidGUI(this, Vehicle, forces, steering_angles)
@@ -363,45 +350,41 @@ classdef bullet_interface < handle
         disp(' || q:     quit');
         disp('----------------------------------------------');
         set(0, 'DefaultFigurePosition', [10,10,900,900]);
-        this.gui.fig = figure;
-        set(this.gui.fig, 'KeyPressFcn', {@figure_keypress, this});
-        view(3);
-        hold all;
-        xlabel('x'); ylabel('y'); zlabel('z');
-        title(['Bullet simulation (paused)', char(10), 'Timestep: ',...
-               num2str(this.gui.timestep), char(10), 'Framerate: ', ...
-               num2str(this.gui.framerate)]);
-        for i = 1:numel(this.Terrain),
-            this.Terrain(i).Draw;
-        end
-        this.DrawSimulation();
+        % this.gui.fig = figure;
+        % set(this.gui.fig, 'KeyPressFcn', {@figure_keypress, this});
+        % view(3);
+        % hold all;
+        % xlabel('x'); ylabel('y'); zlabel('z');
+        % title(['Bullet simulation (paused)', char(10), 'Timestep: ',...
+        %        num2str(this.gui.timestep), char(10), 'Framerate: ', ...
+        %        num2str(this.gui.framerate)]);
+        % for i = 1:numel(this.Terrain),
+        %     this.Terrain(i).Draw;
+        % end
+        % this.DrawSimulation();
     end
 
     %%%% RUNNING THE SIMULATION
     %%%% Handles the interaction between the Bullet Physics engine and the Sim.
 
-    function RunSimulation(this)
-%         if ~this.isSceneGraphOn, 
-            if this.gui.draw,
-                title(['Bullet simulation (running)',char(10),'Timestep: ',...
-                       num2str(this.gui.timestep), char(10), 'Framerate: ', ...
-                       num2str(this.gui.framerate), '  fps']);
-                this.StepSimulation();
-            elseif this.gui.iter == true,
-                title(['Bullet simulation (paused)',char(10),'Timestep: ',...
-                       num2str(this.gui.timestep), char(10), 'Framerate: ', ...
-                       num2str(this.gui.framerate), '  fps']);
-                this.StepSimulation();
-                this.gui.iter = false;
-            else
-                title(['Bullet simulation (paused)',char(10),'Timestep: ',...
-                       num2str(this.gui.timestep), char(10),'Framerate: ', ...
-                       num2str(this.gui.framerate), '  fps']);
-            end      
-            this.DrawSimulation();
-%         else
-%             bulletInterface('RunSceneGraph', this.bulletHandle);
-%         end
+    function RunSimulation(this) 
+        % if this.gui.draw,
+        %     title(['Bullet simulation (running)',char(10),'Timestep: ',...
+        %            num2str(this.gui.timestep), char(10), 'Framerate: ', ...
+        %            num2str(this.gui.framerate), '  fps']);
+            this.StepSimulation();
+        % elseif this.gui.iter == true,
+        %     title(['Bullet simulation (paused)',char(10),'Timestep: ',...
+        %            num2str(this.gui.timestep), char(10), 'Framerate: ', ...
+        %            num2str(this.gui.framerate), '  fps']);
+        %     this.StepSimulation();
+        %     this.gui.iter = false;
+        % else
+        %     title(['Bullet simulation (paused)',char(10),'Timestep: ',...
+        %            num2str(this.gui.timestep), char(10),'Framerate: ', ...
+        %            num2str(this.gui.framerate), '  fps']);
+        % end      
+        % this.DrawSimulation();
     end
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -409,9 +392,9 @@ classdef bullet_interface < handle
     
     %%%% Updates all of our objects according to the Bullet world
     function StepSimulation(this)
-      bulletInterface('StepSimulation', this.bulletHandle);
+      buckshot('StepSimulation', this.bulletHandle);
       for i = 1:numel(this.Shapes),
-        [position, rotation] = bulletInterface('GetTransform', ...
+        [position, rotation] = buckshot('GetTransform', ...
           this.bulletHandle, 'Shape', this.Shapes{i}.GetID());
         this.Shapes{i}.SetTransform(position, rotation);
       end
@@ -428,7 +411,7 @@ classdef bullet_interface < handle
             wheel_fl_pos, wheel_fl_rot,...
             wheel_fr_pos, wheel_fr_rot,...
             wheel_bl_pos, wheel_bl_rot,...
-            wheel_br_pos, wheel_br_rot] = bulletInterface('GetTransform', ...
+            wheel_br_pos, wheel_br_rot] = buckshot('GetTransform', ...
             this.bulletHandle, 'RaycastVehicle', this.RayVehicles{i}.GetID());
           this.RayVehicles{i}.SetTransform(body_position, body_rotation,...
             wheel_fl_pos, wheel_fl_rot,...
@@ -442,7 +425,7 @@ classdef bullet_interface < handle
       end
       for i = 1:numel(this.Constraints),
         if strcmp(this.Constraints{i}.GetType(), 'Hinge2')==true,
-          [position] = bulletInterface('GetTransform', ...
+          [position] = buckshot('GetTransform', ...
             this.bulletHandle, 'Constraint', this.Constraints{i}.GetID());
           this.Constraints{i}.SetPosition(position);
         end
@@ -464,15 +447,15 @@ classdef bullet_interface < handle
       h_constraints = [];
       h_rayvehicles = [];
       for i = 1:numel(this.Shapes),
-        h_shapes = [h_shapes, this.Shapes{i}.Draw];
+          h_shapes = [h_shapes, this.Shapes{i}.Draw];
       end
       for i = 1:numel(this.RayVehicles)
-        h_rayvehicles = [h_rayvehicles, this.RayVehicles{i}.Draw()];
+          h_rayvehicles = [h_rayvehicles, this.RayVehicles{i}.Draw()];
       end
       if this.gui.draw_constraint == true,
-        for i = 1:numel(this.Constraints),
-          h_constraints = [h_constraints, this.Constraints{i}.Draw];
-        end
+          for i = 1:numel(this.Constraints),
+              h_constraints = [h_constraints, this.Constraints{i}.Draw];
+          end
       end
       %If you want to access the cameraPosition, do it through the axes options
       axis equal;
