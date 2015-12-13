121854b0447b550d27ec8dc1a779d006f831bba2
bminortx <gallimatrix@gmail.com>
2015-12-12 22:14:19 -0700

    Full demo now

--- Demos/BulletDemo.m
+++ Demos/BulletDemo.m
@@ -18,14 +18,14 @@ Map = Terrain(50, 50, 3, 1);
 %%% A. It's easier to initialize them all at one time,
 %%% B. They are easier to access when adding constraints
 %%% Directory: Matlab_shapes
-% Shapes{1} = Sphere(3, 5, 2);
-% Shapes{1}.SetPosition([10, 10, 8]);
-% Shapes{2} = Cube(10, 10, 1, 1, 2);
-% Shapes{2}.SetPosition([0, 0, 2]);
-% Shapes{3} = Sphere(1, 5, 2);
-% Shapes{3}.SetPosition([-10, 10, 8]);
-% Shapes{4} = Sphere(1, 5, 2);
-% Shapes{4}.SetPosition([-6, 8, 8]);
+Shapes{1} = Sphere(3, 5, 2);
+Shapes{1}.SetPosition([10, 10, 8]);
+Shapes{2} = Cube(10, 10, 1, 1, 2);
+Shapes{2}.SetPosition([0, 0, 2]);
+Shapes{3} = Sphere(1, 5, 2);
+Shapes{3}.SetPosition([-10, 10, 8]);
+Shapes{4} = Sphere(1, 5, 2);
+Shapes{4}.SetPosition([-6, 8, 8]);
 
 %%% CREATE CONSTRAINTS Place your constraints in a cell matrix, for
 %%% the same reason you did it for your shapes.  Directory:
@@ -33,19 +33,24 @@ Map = Terrain(50, 50, 3, 1);
 %%% constructor, depending on the number of shapes you want to
 %%% contrain, or the type of data you want to use to identify the
 %%% constraint.
-% Constraints{1} = Hinge(Shapes{1}, [4, 0, 0], [0, 1, 0]);
-% Constraints{1}.SetLimits(3, -3);
-% Constraints{2} = SixDOF(Shapes{3}, Shapes{4}, ...
-%                         [1, 1, 0, 1, 0, 1], [-1, 1, 0, -1, 0, -1]);
-% Constraints{3} = PointToPoint(Shapes{3},[1, 1, 1]);
+Constraints{1} = Hinge(Shapes{1}, [4, 0, 0], [0, 1, 0]);
+Constraints{1}.SetLimits(3, -3);
+Constraints{2} = SixDOF(Shapes{3}, Shapes{4}, ...
+                        [1, 1, 0, 1, 0, 1], [-1, 1, 0, -1, 0, -1]);
+Constraints{3} = PointToPoint(Shapes{3},[1, 1, 1]);
 
 %%%% ADD OUR OBJECTS TO BUCKSHOT
 BuckshotSim.AddTerrain(Map);
-% BuckshotSim.AddShapes(Shapes);
-% BuckshotSim.AddConstraints(Constraints);
+disp('done')
+BuckshotSim.AddShapes(Shapes);
+disp('done')
+BuckshotSim.AddConstraints(Constraints);
+disp('done')
 
 % %%%% RUN THE GUI
-% BuckshotSim.InitSimulation();
-% while(BuckshotSim.gui.quit==false), 
-%   BuckshotSim.RunSimulation;
-% end
+BuckshotSim.InitSimulation();
+disp('done')
+while(BuckshotSim.gui.quit==false), 
+  BuckshotSim.RunSimulation;
+end
+disp('done')
